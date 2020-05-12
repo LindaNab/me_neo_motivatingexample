7f03b657712a747e67b91199267f2ac8d7e5b196
Linda Nab <l.nab@lumc.nl>
2020-05-12 23:32:50 +0200

    change the dir of scatterhist

---
+++ rcode/visualisation/create_scatterhist.R
@@ -0,0 +1,74 @@
+#############################################################
+## Internal validation sampling strategies for exposure 
+## measurement error correction 
+## Motivating example
+##
+## Create scatterhistograms to illustrate sampling strategies
+## lindanab4@gmail.com - 20200508
+#############################################################
+
+##############################
+# 0 - Load librairies
+##############################
+library(data.table)
+source("./rcode/analyses/sample_valdata.R") # needed for bins uniform sampling
+source("./rcode/visualisation/scatterhistogram.R") # code of scatterhistogram
+
+############################## 
+# 1 - Prepare data for visualisation
+##############################
+data_path <- "./data/processed" 
+data <- readRDS(file = paste0(data_path, "/ldrp.Rds"))
+# use data for analysis
+
+data_random <- select_valdata(
+  data = data,
+  use_variable = NA,
+  size_valdata = 0.4,
+  sampling_strat = "random",
+  seed = 20200508
+)
+data_uniform <- select_valdata(
+  data = data,
+  use_variable = "z_middelomtrek",
+  size_valdata = 0.4,
+  sampling_strat = "uniform",
+  seed = 20200508
+)
+data_extremes <- select_valdata(
+  data = data,
+  use_variable = "z_middelomtrek",
+  size_valdata = 0.4,
+  sampling_strat = "extremes",
+  seed = 20200508
+)
+# make bins for uniform plot
+# total number of subjects in data
+n <- NROW(data)
+# desired  number of subjects in valdata
+n_valdata <- ceiling(n * 0.4)
+n_bins <- 10
+n_each_bin <- round(n_valdata / n_bins) # possibly less people included in
+# validation sample due to rounding
+bins <-
+  create_bins(n_bins, 
+              n_each_bin, 
+              data, 
+              use_variable = "z_middelomtrek", 
+              n_valdata)
+
+##############################
+# 2 - Create and save scatterhistograms
+##############################
+png(paste0("./results/figures", "/scatterhist_random.png"),
+    width = 6, height = 4, units = 'in', res = 250)
+create_scatterhist(data_random, bins = bins)
+dev.off()
+png(paste0("./results/figures", "/scatterhist_uniform.png"),
+    width = 6, height = 4, units = 'in', res = 250)
+create_scatterhist(data_uniform, uniform = T, bins = bins)
+dev.off()
+png(paste0("./results/figures", "/scatterhist_extremes.png"),
+    width = 6, height = 4, units = 'in', res = 250)
+create_scatterhist(data_extremes, bins = bins)
+dev.off()
--- rcode/visualisation/scatterhistogram.R
+++ rcode/visualisation/scatterhistogram.R
@@ -3,48 +3,20 @@
 ## measurement error correction 
 ## Motivating example
 ##
-## Graphs to illustrate sampling strategies
-## lindanab4@gmail.com - 20200508
+## Scatter histogram
+## lindanab4@gmail.com - 202005012
 #############################################################
 
 ##############################
 # 0 - Load librairies
 ##############################
-library(data.table)
-source("./rcode/analyses/sample_valdata.R")
 
 ############################## 
-# 1 - Prepare data for analysis
+# 1 - Workhorse scatterhistogram
 ##############################
-data_path <- "./data/processed" 
-data <- readRDS(file = paste0(data_path, "/ldrp.Rds"))
-# use data for analysis
-
-data_random <- select_valdata(
-  data = data,
-  use_variable = NA,
-  size_valdata = 0.4,
-  sampling_strat = "random",
-  seed = 20200508
-)
-data_uniform <- select_valdata(
-  data = data,
-  use_variable = "z_middelomtrek",
-  size_valdata = 0.4,
-  sampling_strat = "uniform",
-  seed = 20200508
-)
-data_extremes <- select_valdata(
-  data = data,
-  use_variable = "z_middelomtrek",
-  size_valdata = 0.4,
-  sampling_strat = "extremes",
-  seed = 20200508
-)
-
-
 create_scatterhist <- function(data, 
-                               uniform = F){
+                               uniform = F,
+                               bins){
   zones <- matrix(c(2, 0, 1, 3), ncol = 2, byrow = TRUE)
   layout(zones, widths = c(4/5, 1/5), heights = c(1/5, 4/5))
   par(mar = c(3, 3, 1, 1))
@@ -105,17 +77,3 @@ create_scatterhist <- function(data,
             xlim = c(0, top),
             space = 0, horiz = T)
 }
-
-png(paste0("./results/figures", "/scatterhist_random.png"),
-    width = 6, height = 4, units = 'in', res = 250)
-create_scatterhist(data_random)
-dev.off()
-png(paste0("./results/figures", "/scatterhist_uniform.png"),
-    width = 6, height = 4, units = 'in', res = 250)
-create_scatterhist(data_uniform, uniform = T)
-dev.off()
-png(paste0("./results/figures", "/scatterhist_extremes.png"),
-    width = 6, height = 4, units = 'in', res = 250)
-create_scatterhist(data_extremes)
-dev.off()
-      
